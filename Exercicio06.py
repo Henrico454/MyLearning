19b37160b7298131498d6845c8e8bd6774024bad
Gabriel Rocha <81485278+Henrico454@users.noreply.github.com>
2021-03-30 14:08:44 -0300

    FirstPythonProgram
    Um exercício para dizer o antecessor e o sucessor de um determinado número

---
+++ Exercicio06.py
@@ -0,0 +1,6 @@
+# Programa feito para dizer qual é o antecessor e o sucessor do determinado número
+# programa simples e já executado
+num1 = int(input('Digite um número:'))
+a = num1 - 1
+p = num1 + 1
+print('O número é {}. O seu antecessor é igual a {}. e seu sucessor é igual a {}.'.format(num1, a,  p))
