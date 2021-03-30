b8e15c91059b3a5ffcd4a0b990ed26bc73d4c087
Gabriel Rocha <81485278+Henrico454@users.noreply.github.com>
2021-03-30 14:18:07 -0300

    ThirdPythonProgram
    Um programa para calcular media. Digite suas notas e o programa irá calcular e te dizer sua média

---
+++ Exercicios08.py
@@ -0,0 +1,6 @@
+# Programa para fazer o calculo de duas notas de um aluno
+# calcular e mostrar a media do aluno
+nota1 = float(input('Digite a sua primeira nota: '))
+nota2 = float(input('Digite a sua segunda nota'))
+media = (nota1 + nota2) /2
+print('Sua média foi de: {}'.format(media))
